ca403a8887125e09a3d1215b5e2cb06ddfa8f94c
Muskan044 <muskankhan430430@gmail.com>
2024-07-07 17:44:21 +0500

    add solutions

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 // table
-var num = +prompt("enter a number");
+// var num = +prompt("enter a number");
 
-for (var i = 1; i <= 10; i++) {
-  console.log(`${num} * ${i} = ${num * i}`);
-}
+// for (var i = 1; i <= 10; i++) {
+//   console.log(`${num} * ${i} = ${num * i}`);
+// }
 
+// sum of numbers
+// var numbers = []
+// var sum = 0
+// for (var i = 0; i < 5; i++){
+//     var numb = +prompt("enter a number: ")
+//     numbers.push(numb)
+// }
+// for (var i = 0; i < 5; i++){
+//   sum = sum + numbers[i];
+// }
+// console.log(sum)
+
+// largest
+// var numbers = [5, 3, 7, 10]
+// var largest = numbers[0]
+
+// for (var i = 0; i < numbers.length; i++){
+//     if(numbers[i] > largest){
+//       largest - numbers[i]
+//     }
+// }
+// console.log(largest)
+
+// // reversed
+// var name = "muskan"
+// var reversed = ""
+
+// for(var i = name.length-1; i >= )
 
 
 
